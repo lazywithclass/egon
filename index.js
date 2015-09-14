5325972fc973fde49e8b35571e73747ddf0a19ec
Alberto Zaccagni <montecristoh@gmail.com>
2015-09-14 15:20:33 +0100

    fixed typo

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var AWS = require('aws-sdk');
 AWS.config.region = 'us-east-1';
 var cloudwatchlogs = new AWS.CloudWatchLogs();
 
-module.exports.crossStreams = require('./lib/crossStreams')
+module.exports.crossStreams = require('./lib/cross-streams')
   .crossStreams.bind(null, cloudwatchlogs);
