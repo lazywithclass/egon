2952ad54b0b8db5b53037311726120833ffd5cd8
Alberto Zaccagni <montecristoh@gmail.com>
2015-09-14 11:38:50 +0100

    first working spike

--- index.js
+++ index.js
@@ -1,39 +1,49 @@
 var AWS = require('aws-sdk'),
-    logGroupName = '/aws/lambda/stringify',
-    events = require('events'),
-    eventEmitter = new events.EventEmitter();
+    async = require('async');
 
 AWS.config.region = 'us-east-1';
 var cloudwatchlogs = new AWS.CloudWatchLogs();
+var tokens = {};
 
-var params = { logGroupName: logGroupName };
+function unify(logGroupName, cb) {
+  cloudwatchlogs.describeLogStreams({ logGroupName: logGroupName }, function(err, data) {
+    if (err) return cb(err);
 
-cloudwatchlogs.describeLogStreams(params, function(err, data) {
-  if (err) return console.log(err, err.stack);
-  data.logStreams.forEach(function(logStream) {
-    run(logGroupName, logStream.logStreamName);
-  });
-});
-
-function run(logGroupName, logStreamName, nextToken) {
-  var params = {
-    logGroupName: logGroupName,
-    logStreamName: logStreamName,
-    nextToken: nextToken
-  };
-
-  cloudwatchlogs.getLogEvents(params, function(err, log) {
-    if (err) return console.log(err);
-    if (!log) return wait(nextToken, run);
-
-    eventEmitter.emit('events', logStreamName, log.events);
-    wait(run.bind(null, logGroupName, logStreamName, log.nextForwardToken));
+    var allEvents = [];
+    async.each(
+      data.logStreams,
+      function(logStream, cb) {
+        run(tokens, logGroupName, logStream.logStreamName, function(err, events) {
+          allEvents = allEvents.concat(events);
+          cb();
+        });
+      },
+      function(err) {
+        allEvents.sort(function(a,b) {
+          if (a.timestamp < b.timestamp) return -1;
+          if (a.timestamp > b.timestamp) return 1;
+          return 0;
+        });
+        cb(null, allEvents);
+      }
+    );
   });
 }
 
-function wait(cb) { setTimeout(cb, 2000); }
+function run(tokens, logGroupName, logStreamName, cb) {
+  var params = {
+    logGroupName: logGroupName,
+    logStreamName: logStreamName,
+    nextToken: tokens[logStreamName]
+  };
 
+  cloudwatchlogs.getLogEvents(params, function(err, log) {
+    if (err) console.log(err);
+    if (!log) return cb(null, []);
 
-eventEmitter.on('events', function(a,b) {
-  console.log(a,b)
-});
+    tokens[logStreamName] = log.nextForwardToken;
+    cb(null, log.events);
+  });
+}
+
+module.exports = unify;
